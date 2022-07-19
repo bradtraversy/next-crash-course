01a043ddc7c1bf7033db8637b373835114be3bb1
xgqfrms <xgqfrms@outlook.com>
2022-07-19 17:53:49 +0800

    feat: use `find` instead of `filter`, which is more concise

--- pages/api/articles/[id].js
+++ pages/api/articles/[id].js
@@ -1,10 +1,10 @@
 import { articles } from '../../../data'
 
 export default function handler({ query: { id } }, res) {
-  const filtered = articles.filter((article) => article.id === id)
+  const filtered = articles.find((article) => article.id === id)
 
-  if (filtered.length > 0) {
-    res.status(200).json(filtered[0])
+  if (filtered) {
+    res.status(200).json(filtered)
   } else {
     res
       .status(404)
